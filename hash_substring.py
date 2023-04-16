9aa393e9953042059a47932a230b7cc707121419
Jānis Kļaviņš <124731554+klavinsj@users.noreply.github.com>
2023-04-16 15:03:28 +0000

    cerams

--- hash_substring.py
+++ hash_substring.py
@@ -11,9 +11,15 @@ def read_input():
     # second line is text in which to look for pattern 
     
     # return both lines in one return
-    
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    izvele = input()
+    if "F" in izvele:
+        fails = input()
+        fails = "./tests/" + fails
+        ievade = open(fails,"r").readlines()
+        return (ievade[0].rstrip(), ievade[1].rstrip())
+    if "I" in izvele:
+        return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,10 +27,41 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    result = []
+    d = 256
+    num = 101
 
+    P = len(pattern)
+    T = len(text)
+
+    x = 0
+    l = 0
+    h = 1
+
+    for i in range(P - 1):
+        h = (h * d) % num
+
+    for i in range(P):
+        x = (d * x + ord(pattern[i])) % num
+        l = (d * l + ord(text[i])) % num
+
+    for i in range(T - P + 1):
+        if x == l:
+            for j in range(P):
+                if text[i + j] != pattern[j]:
+                    break
+                else:
+                    j += 1
+            if j == P:
+                result.append(i)
+        if i < T - P:
+            l = (d * (l - ord(text[i])*h) + ord(text[i+P])) % num
+            if l < 0:
+                l = l + num
+                
     # and return an iterable variable
-    return [0]
-
+    return result
+    # and return an iterable variable
 
 # this part launches the functions
 if __name__ == '__main__':
